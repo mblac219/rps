a66087e429f3d1381c2263ce5e4eb13c5a8afbf5
Michael Blackson <mkblcksn@gmail.com>
2023-07-31 01:13:46 -0500

    Prompt for user and random play for computer

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 
 //function for random computer play
 function getComputerPlay() {
-  const choices = ["r", "p", "s"];
+  const choices = ["rock", "paper", "scissors"];
   const randomChoices = choices[Math.floor(Math.random() * choices.length)];
   return randomChoices;
 }
@@ -15,18 +15,23 @@ function playRound(playerSelection, computerSelection) {
 
   if (player === computer) {
     return "It's a draw!";
-  } else if (player == "r" && computer == "s") {
+  } else if (player == "rock" && computer == "scissors") {
     return "You win! Rock beats Scissors";
-  } else if (player == "s" && computer == "r") {
+  } else if (player == "scissors" && computer == "rock") {
     return "Computer win! Rock beats Scissors";
-  } else if (player == "p" && computer == "r") {
+  } else if (player == "paper" && computer == "rock") {
     return "You win! Paper beats Rock";
-  } else if (player == "r" && computer == "p") {
+  } else if (player == "rock" && computer == "paper") {
     return "Computer win! Paper beats Rock";
-  } else if (player == "s" && computer == "p") {
+  } else if (player == "scissors" && computer == "paper") {
     return "You win! Scissors beat Paper";
-  } else if (player == "p" && computer == "s") {
+  } else if (player == "paper" && computer == "scissors") {
     return "Computer win! Scissors beat Paper";
   }
-  console.log(playRound(playerSelection, computerSelection));
 }
+
+//Prompts user to choose R,P,S
+const playerSelection = prompt("rock, paper, or scissors?");
+//Computer makes a random selection between R,P,S
+const computerSelection = getComputerPlay();
+console.log(playRound(playerSelection, computerSelection));
